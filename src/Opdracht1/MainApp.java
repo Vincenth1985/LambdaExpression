3d4ddfc58c8787689300962045fb368c06bb583c
Vincent Honca <vincenth1985@gmail.com>
2019-06-01 19:49:37 +0200

    Woorden met eerste letter a

--- src/Opdracht1/MainApp.java
+++ src/Opdracht1/MainApp.java
@@ -8,7 +8,21 @@ public class MainApp {
         TextPrinter textPrinter = new TextPrinter("Hello this are my Lambdas");
 
 
+        System.out.println("Woorden met letter 'e'");
+        System.out.println("-".repeat(30));
         textPrinter.filteredWord(s -> s.contains("e"));
+        System.out.println();
+
+
+        System.out.println("Woorden met meer letters dan 4");
+        System.out.println("-".repeat(30));
+        textPrinter.filteredWord(s -> s.length() > 4);
+        System.out.println();
+
+
+        System.out.println("Woorden met eerste letter a");
+        System.out.println("-".repeat(30));
+        textPrinter.filteredWord(s -> s.charAt(0) == 'a');
 
 
     }
