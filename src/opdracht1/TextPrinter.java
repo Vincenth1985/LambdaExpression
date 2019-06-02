8d11d9673ad3d757f6a71bc6eacb576a5f7d925d
Vincent Honca <vincenthonca@iMac-de-Vincent.lan>
2019-06-02 18:21:30 +0200

    Vervang de printNumberValues en PrintProcessedWords met de Function interface

--- src/opdracht1/TextPrinter.java
+++ src/opdracht1/TextPrinter.java
@@ -29,7 +29,7 @@ public class TextPrinter {
         return null;
     }
 
-    public void printProcessedWords(Function<String,String> wordProcessor) {
+    public void printProcessedWords(Function<String, String> wordProcessor) {
         for (String s : sentence.split(" ")) {
             //Vervang van mijn eigen WordProcessor functional interface door Function interface met aanroep naar de apply methode die een een object van een bepaalde type terug geeft in een andere type.We gebruiken hier Java.util.function interfaces.
             System.out.println(wordProcessor.apply(s));
@@ -38,17 +38,16 @@ public class TextPrinter {
 
     }
 
-    public void printNumberValues(NumberParser numberParser) {
+    public void printNumberValues(Function<String, BigDecimal> numberParser) {
         for (String s : sentence.split(" ")) {
-            System.out.println(numberParser.parse(s));
+            System.out.println(numberParser.apply(s));
         }
     }
 
-    public void printSum(NumberParser parser) {
+    public void printSum(Function<String, BigDecimal> parser) {
         int x = 0;
         for (String s : sentence.split(" ")) {
-            x += parser.parse(s).toBigInteger().intValue();
-
+            x += parser.apply(s).toBigInteger().intValue();
         }
         System.out.println(x);
     }
